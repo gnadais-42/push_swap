5894eebb04afbd26cf8da88e8ad305807512149c
gnadais-42 <guilhermenadaispinho@gmail.com>
2025-12-11 00:32:41 +0000

    advancements in parser

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: gnadais- <gnadais-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 16:50:38 by gnadais-          #+#    #+#             */
-/*   Updated: 2025/12/10 17:15:33 by gnadais-         ###   ########.fr       */
+/*   Updated: 2025/12/11 00:32:05 by gnadais-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,34 @@ static int	is_repetition(int number, t_list *args);
 
 t_list	parser(int arglen, char *args[])
 {	
-	t_list	parsed;
-	int		i;
+	t_list	*parsed;
+	t_list	*node;
+	int		*content;
 
-	i = 0;
-
+	parsed = NULL;
+	if (arglen == 2)
+		args = ft_split(*(++args), ' ');
+	while (*args)
+	{
+		if (!validator(*args, parsed))
+			exit_program();
+		content = malloc(sizeof(int));
+		if (!content)
+		{
+			ft_lstclear(&parsed, del);
+			exit_program();
+		}
+		*content = ft_atoi(*args);
+		node = ft_lstnew(content);
+		if (!node)
+		{
+			ft_lstclear(&parsed, del);
+			exit_program();
+		}
+		ft_lstadd_back(&parsed, node);
+		args++;
+	}
+	return (parsed);
 }
 
 static void exit_program(void)
@@ -31,7 +54,7 @@ static void exit_program(void)
 	exit(1);
 }
 
-static int	validator(char *argument, t_list *args)
+static int	validator(char *argument, t_list *llist)
 {
 	int	number;
 
@@ -43,18 +66,18 @@ static int	validator(char *argument, t_list *args)
 		if (!ft_isdigit(*argument++))
 			return (0);
 	}
-	if (is_repetition(number, args))
+	if (is_repetition(number, llist))
 		return (0);
 	return (1);
 }
 
-static int	is_repetition(int number, t_list *args)
+static int	is_repetition(int number, t_list *llist)
 {
-	while (args)
+	while (llist)
 	{
-		if (args->content && *(int *)args->content == number)
+		if (llist->content && *(int *)llist->content == number)
 			return (1);
-		args = args->next;
+		llist = llist->next;
 	}
 	return (0);
 }
